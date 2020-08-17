0c9de90f02fcd3f77a3ccc4321809f41cfe324b9
TestProgger <gamersteam_dubki@mail.ru>
2020-08-17 19:25:08 +0300

    Add files

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+const fs = require("fs");
+
+// Const variables
+const port = 3000;
+const sourceFile = "file.doc";
+const secretKey = "secret-key";
+
+// Configs
+app.use(helmet());
+app.use(cookieParser(secretKey));
+
+// Routes
+app.get("/", async (request, response) => {
+  const newFileName = request.query.filename;
+
+  if (!newFileName) {
+    response.statusCode = 404;
+    response.send(
+      `<h1>Error: filename is empty</h1> <p>Example: http://localhost:${port}/?filename=blablabla</p>`
+    );
+    return;
+  }
+
+  response.cookie(
+    "referrer",
+    request.headers.referer || `Referer_not_found_${request.url}`,
+    {
+      expires: new Date(Date.now() + 3600 * 1000),
+      httpOnly: true,
+      secure: true,
+      signed: true,
+    }
+  );
+  response.download(sourceFile, newFileName, (error) => {
+    if (error) {
+      response.statusCode = 404;
+      response.send("<h1>Resourse not found</h1>");
+      return;
+    }
+  });
+});
+
+app.listen(port);
